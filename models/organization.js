5a1e85c9e77c8f23801f4d4424ac333270c67632
kingaranjoroge <111523863+kingaranjoroge@users.noreply.github.com>
2023-12-12 00:04:32 +0300

    added orgainzation's database table

---
+++ models/organization.js
@@ -0,0 +1,40 @@
+const Sequelize = require('sequelize');
+
+const sequelize = require('../db/connection');
+
+const Organization = sequelize.define('Organization', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true
+  },
+  location: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  departments: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true
+  },
+  contactNumber: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true
+  },
+  availablePositions: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+});
+
+module.exports = Organization;
