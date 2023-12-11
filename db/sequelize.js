62fd840b028c438e4e63bb1966cce5d95465bcd7
Raccoon254 <tomsteve187@gmail.com>
2023-12-11 23:35:48 +0300

    Database Config

--- db/sequelize.js
+++ db/sequelize.js
@@ -1,7 +1,11 @@
 const { Sequelize } = require('sequelize');
+const DB_NAME = process.env.DB_NAME || 'interlink';
+const DB_USER = process.env.DB_USER || 'root';
+const DB_PASSWORD = process.env.DB_PASSWORD || '';
+const DB_HOST = process.env.DB_HOST || 'localhost';
 
-const sequelize = new Sequelize('internlink', 'root', '', {
-    host: 'localhost',
+const sequelize = new Sequelize('DB_NAME', 'DB_USER', 'DB_PASSWORD', {
+    host: DB_HOST,
     dialect: 'mysql',
     logging: true,
 });
