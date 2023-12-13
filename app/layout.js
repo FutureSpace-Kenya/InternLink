b21a654f25d0e73c677addfd2064e3165dd4c277
Raccoon254 <tomsteve187@gmail.com>
2023-12-13 08:35:05 +0300

    Added Font awesome

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,20 @@
-import { Inter } from 'next/font/google'
+import {Inter} from 'next/font/google'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({subsets: ['latin']})
 
 export const metadata = {
-  title: 'InternLink',
-  description: 'A platform for connecting interns and attaches to companies',
+    title: 'InternLink',
+    description: 'A platform for connecting interns and attaches to companies',
 }
 
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en" data-theme={'winter'}>
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+export default function RootLayout({children}) {
+    return (
+        <html lang="en" data-theme={'winter'}>
+        <body className={inter.className}>
+            {children}
+            <script src="https://kit.fontawesome.com/af6aba113a.js" crossOrigin="anonymous"></script>
+        </body>
+        </html>
+    )
 }
