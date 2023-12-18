6671fe07b50a7b639bb9d6b3abb6b0d1aa6ded77
Raccoon254 <tomsteve187@gmail.com>
2023-12-18 23:55:36 +0300

    Config fix

--- db/sequelize.js
+++ db/sequelize.js
@@ -1,13 +1,14 @@
 const { Sequelize } = require("sequelize");
 const DB_NAME = process.env.DB_NAME || "interlink";
-const DB_USER = process.env.DB_USER || "internlink_test";
+const DB_USER = process.env.DB_USER || "root";
 const DB_PASSWORD = process.env.DB_PASSWORD || "";
 const DB_HOST = process.env.DB_HOST || "localhost";
 
-const sequelize = new Sequelize("DB_NAME", "DB_USER", "DB_PASSWORD", {
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
   dialect: "mysql",
   logging: true,
+  dialectModule: require('mysql2'),
 });
 
 module.exports = sequelize;
