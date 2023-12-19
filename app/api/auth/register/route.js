8654f2e8eb4735b518e5dc81790bcde198529b03
Raccoon254 <tomsteve187@gmail.com>
2023-12-19 12:08:20 +0300

    Added env to gitignore

--- app/api/auth/register/route.js
+++ app/api/auth/register/route.js
@@ -1,9 +1,10 @@
 import {NextResponse} from "next/server";
+import * as request from "sequelize";
 
 export async function GET() {
     return NextResponse.json({message: 'Hello from Next.js!'})
 }
 
 export async function POST() {
-    return NextResponse.json({message: 'Hello from Next.js!'})
+    let body = await request.json()
 }
