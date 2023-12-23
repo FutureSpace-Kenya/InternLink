fa4f533784740bf5c95bf3b5dc242b97682bc36a
Raccoon254 <tomsteve187@gmail.com>
2023-12-23 21:08:49 +0300

    Session detection for current user

--- app/auth/login/page.jsx
+++ app/auth/login/page.jsx
@@ -2,12 +2,12 @@
 import Link from "next/link";
 import LoginButton from "../../components/LoginButton";
 import React, {useEffect, useState} from "react";
-import {signIn, useSession} from "next-auth/react";
+import {signIn, signOut, useSession} from "next-auth/react";
 import {redirect} from "next/navigation";
 import Notification from "/app/Notification";
 
 export default function Login() {
-    const { data: session } = useSession()
+    const {data: session} = useSession()
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [emailError, setEmailError] = useState('');
@@ -112,102 +112,128 @@ export default function Login() {
                             </div>
                         </div>
                     </center>
-                    <div className="w-full mt-8">
-                        <p>
-                            You are already signed in as {session.user.email}
-                        </p>
-                        <LoginButton/>
+                    <div className="w-full flex justify-center items-center flex-col mt-8">
+                        <center>
+                            <div className="flex w-full items-center justify-center rounded-lg bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+                                <div className="max-w-md w-full space-y-8">
+                                    <div>
+                                        <div className="avatar">
+                                            <i className={'fas fa-user-circle fa-5x'}></i>
+                                        </div>
+                                        <h2 className="mt-6 text-center text-3xl font-bold text-gray-900">
+                                           You are signed in as
+                                        </h2>
+                                        <p className="mt-2 text-center text-sm text-gray-600">
+                                            {session.user.email}
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </center>
+                        <div className="flex mt-4 flex-col sm:flex-row w-9/12 gap-3 items-center justify-between">
+                            <div className="w-full">
+                                <button className={'btn btn-outline text-red-600 ring-2  ring-offset-1 w-full'}  onClick={() => signOut()}>
+                                    Sign Out <i className="fas fa-sign-out-alt"> </i>
+                                </button>
+                            </div>
+                            <Link className={'w-full'} href={'/intern/dashboard'}>
+                                <button className={'btn btn-outline btn-secondary ring-2  ring-offset-1 w-full'}>
+                                    Go to Dashboard <i className="fas fa-arrow-right"> </i>
+                                </button>
+                            </Link>
+                        </div>
                     </div>
-                    </div>
+                </div>
             </main>
-    );
+        );
     }
 
-return (
-    <main className="min-h-screen grid place-items-center w-full">
-        <div className="w-full max-w-md m-4 p-4 ">
-            <Notification notifications={notifications}/>
-            <center>
-                <div className="w-fit relative flex flex-col items-center">
-                    <h2 className="">
+    return (
+        <main className="min-h-screen grid place-items-center w-full">
+            <div className="w-full max-w-md m-4 p-4 ">
+                <Notification notifications={notifications}/>
+                <center>
+                    <div className="w-fit relative flex flex-col items-center">
+                        <h2 className="">
                             <span className="text-green-400">
                                  Intern
                             </span>
-                        Link&trade; Auth
-                    </h2>
-                    <div className="absolute top-[35px] right-0 mb-4 text-xs font-medium text-orange-800">
-                        By <a className={'text-blue-500'} href="https://futurespace.vercel.app">FutureSpace</a>
-                    </div>
-                </div>
-            </center>
-
-            <div className="w-full mt-8">
-                <form className="shadow-sm p-6 rounded" onSubmit={handleSubmit}>
-
-                    <div className="mb-4">
-                        <label className="block text-gray-700 text-sm mb-2" htmlFor="email">
-                            Email
-                        </label>
-                        <input className="input input-bordered input-md w-full max-w-md"
-                               id="email" required type="email" placeholder="Email"
-                               value={email}
-                               onChange={(e) => {
-                                   setEmail(e.target.value);
-                                   if (!validateEmail(e.target.value)) {
-                                       setEmailError('Invalid email');
-                                   } else {
-                                       setEmailError('');
-                                   }
-                               }}
-                        />
-                        {emailError && <p className="text-red-500">{emailError}</p>}
-                    </div>
-                    <div className="mb-6">
-                        <label className="block text-gray-700 text-sm mb-2" htmlFor="password">
-                            Password
-                        </label>
-                        <input className="input input-bordered input-md w-full max-w-md" id="password"
-                               type="password" placeholder="******************"
-                               value={password}
-                               onChange={(e) => {
-                                   setPassword(e.target.value);
-                                   if (!validatePassword(e.target.value)) {
-                                       setPasswordError('Password must be at least 8 characters');
-                                   } else {
-                                       setPasswordError('');
-                                   }
-                               }}
-                        />
-                        {passwordError && <p className="text-red-500">{passwordError}</p>}
-                    </div>
-                    <div className="flex flex-col gap-3 items-center justify-between">
-                        <input type={'submit'} className="btn btn-outline btn-secondary ring-2  ring-offset-1 w-full"
-                               value={'Sign In'}/>
-                        <div className="flex w-full items-center justify-center">
-                            {/*Line*/}
-                            <hr className="w-full border-gray-300"/>
-                            {/*Text*/}
-                            <span className="text-gray-500 mx-3">or</span>
-                            {/*Line*/}
-                            <hr className="w-full border-gray-300"/>
+                            Link&trade; Auth
+                        </h2>
+                        <div className="absolute top-[35px] right-0 mb-4 text-xs font-medium text-orange-800">
+                            By <a className={'text-blue-500'} href="https://futurespace.vercel.app">FutureSpace</a>
                         </div>
-                        <LoginButton/>
                     </div>
-                    <div className="flex text-[12px] underline underline-offset-2 gap-3">
-                        <Link className="inline-block align-baseline my-2 text-blue-500 hover:text-blue-800"
-                              href={"/auth/recovery"}>
-                            Forgot Password?
-                        </Link>
-                        <Link className={'inline-block align-baseline my-2 text-blue-500 hover:text-blue-800'}
-                              href={'/auth/register'}>
-                            New User? Sign Up
-                        </Link>
-                    </div>
+                </center>
 
-                </form>
+                <div className="w-full mt-8">
+                    <form className="shadow-sm p-6 rounded" onSubmit={handleSubmit}>
+
+                        <div className="mb-4">
+                            <label className="block text-gray-700 text-sm mb-2" htmlFor="email">
+                                Email
+                            </label>
+                            <input className="input input-bordered input-md w-full max-w-md"
+                                   id="email" required type="email" placeholder="Email"
+                                   value={email}
+                                   onChange={(e) => {
+                                       setEmail(e.target.value);
+                                       if (!validateEmail(e.target.value)) {
+                                           setEmailError('Invalid email');
+                                       } else {
+                                           setEmailError('');
+                                       }
+                                   }}
+                            />
+                            {emailError && <p className="text-red-500">{emailError}</p>}
+                        </div>
+                        <div className="mb-6">
+                            <label className="block text-gray-700 text-sm mb-2" htmlFor="password">
+                                Password
+                            </label>
+                            <input className="input input-bordered input-md w-full max-w-md" id="password"
+                                   type="password" placeholder="******************"
+                                   value={password}
+                                   onChange={(e) => {
+                                       setPassword(e.target.value);
+                                       if (!validatePassword(e.target.value)) {
+                                           setPasswordError('Password must be at least 8 characters');
+                                       } else {
+                                           setPasswordError('');
+                                       }
+                                   }}
+                            />
+                            {passwordError && <p className="text-red-500">{passwordError}</p>}
+                        </div>
+                        <div className="flex flex-col gap-3 items-center justify-between">
+                            <input type={'submit'}
+                                   className="btn btn-outline btn-secondary ring-2  ring-offset-1 w-full"
+                                   value={'Sign In'}/>
+                            <div className="flex w-full items-center justify-center">
+                                {/*Line*/}
+                                <hr className="w-full border-gray-300"/>
+                                {/*Text*/}
+                                <span className="text-gray-500 mx-3">or</span>
+                                {/*Line*/}
+                                <hr className="w-full border-gray-300"/>
+                            </div>
+                            <LoginButton/>
+                        </div>
+                        <div className="flex text-[12px] underline underline-offset-2 gap-3">
+                            <Link className="inline-block align-baseline my-2 text-blue-500 hover:text-blue-800"
+                                  href={"/auth/recovery"}>
+                                Forgot Password?
+                            </Link>
+                            <Link className={'inline-block align-baseline my-2 text-blue-500 hover:text-blue-800'}
+                                  href={'/auth/register'}>
+                                New User? Sign Up
+                            </Link>
+                        </div>
+
+                    </form>
+                </div>
             </div>
-        </div>
 
-    </main>
-)
+        </main>
+    )
 }
