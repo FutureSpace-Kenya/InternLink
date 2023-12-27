36e85c1af353cd86730be6ea6ad46d1419c731fe
kingaranjoroge <111523863+kingaranjoroge@users.noreply.github.com>
2023-12-27 23:48:08 +0300

    created PATCH function for updating a single organization

--- app/api/jobs_listings/organization/[id]/route.js
+++ app/api/jobs_listings/organization/[id]/route.js
@@ -21,35 +21,35 @@ export async function GET(request, { params }) {
     }
 }
 
-// export async function PATCH (request, {params}) {
-//     try {
-//         const { id } = params;
+export async function PATCH (request, {params}) {
+    try {
+        const { id } = params;
 
-//         // Find the organization with the specified ID
-//         const organization = await Organization.findByPk(id);
+        // Find the organization with the specified ID
+        const organization = await Organization.findByPk(id);
 
-//         // Check if the organization was found
-//         if (!organization) {
-//             return NextResponse.json({message: "Organization not found"})
-//         }
+        // Check if the organization was found
+        if (!organization) {
+            return NextResponse.json({message: "Organization not found"})
+        }
 
-//         // Access the organization data from the request body
-//         const { name, location, contact } = await request.json();
+        // Access the organization data from the request body
+        const { name, location, contact } = await request.json();
 
-//         // Update the organization with the new data
-//         await organization.update({
-//             name,
-//             location,
-//             contact
-//         });
+        // Update the organization with the new data
+        await organization.update({
+            name,
+            location,
+            contact
+        });
 
-//         // Send a successful response with the updated organization data
-//         return NextResponse.json({message: "Organization updated successfully!", organization})
-//     } catch (error) {
-//         console.error(error);
-//         return NextResponse.json({message: "Error updating organization", error})
-//     }
-// }
+        // Send a successful response with the updated organization data
+        return NextResponse.json({message: "Organization updated successfully!", organization})
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({message: "Error updating organization", error})
+    }
+}
 
 // export async function DELETE(request, { params }) {
 //     try {
