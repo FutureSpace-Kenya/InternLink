e782f2d1f37d4461449e75cc39801c4db59ad1cc
kingaranjoroge <111523863+kingaranjoroge@users.noreply.github.com>
2023-12-27 22:11:39 +0300

    created model for jobs in jobs_listings

---
+++ models/job.js
@@ -0,0 +1,36 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../db/sequelize');
+
+class Job extends Model {
+  static associate(models) {
+    Job.belongsTo(models.Department, { foreignKey: 'departmentId' });
+  }
+}
+
+Job.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    skills: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    status: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    modelName: 'Job',
+  }
+);
+
+module.exports = Job;
