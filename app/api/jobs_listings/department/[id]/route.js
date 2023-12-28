ebef9fad3ceeafa07a6afb73b8c733188f7dee15
kingaranjoroge <111523863+kingaranjoroge@users.noreply.github.com>
2023-12-28 13:03:08 +0300

    created PATCH function for updating a single department

--- app/api/jobs_listings/department/[id]/route.js
+++ app/api/jobs_listings/department/[id]/route.js
@@ -22,5 +22,33 @@ export async function GET(request, { params }) {
         console.error(error);
         return NextResponse.json({message: "Error fetching department", error})
     }
-  }
-  
+}
+
+export async function PATCH (request, {params}) {
+    try {
+        const { id } = params;
+
+        // Find the department with the specified ID
+        const department = await Department.findByPk(id);
+
+        // Check if the department was found
+        if (!department) {
+            return NextResponse.json({message: "Department not found"})
+        }
+
+        // Access the department data from the request body
+        const { name, organizationId } = await request.json();
+
+        // Update the department with the new data
+        await department.update({
+            name,
+            organizationId
+        });
+
+        // Send a successful response with the updated department data
+        return NextResponse.json({message: "Department updated successfully!", department})
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({message: "Error updating department", error})
+    }
+}
