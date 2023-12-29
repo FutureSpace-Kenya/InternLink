f351bb303dbffd4d641d91b708766709711d1250
kingaranjoroge <111523863+kingaranjoroge@users.noreply.github.com>
2023-12-29 12:11:32 +0300

    updated PATCH function for upating a single function belonging to an organization

--- app/api/jobs_listings/departments/[organizationId]/[departmentId]/route.js
+++ app/api/jobs_listings/departments/[organizationId]/[departmentId]/route.js
@@ -26,4 +26,34 @@ export async function GET(request, { params }) {
   }
 }
 
+// Next.js API route function for updating a department in the specified organization
+export async function PATCH(request, { params }) {
+  try {
+    // Extract organizationId and departmentId from the request parameters
+    const { organizationId, departmentId } = params;
+
+    // Extract updated department data from the request body
+    const { name } = await request.json();
+
+    // Retrieve the specified department belonging to the specified organization
+    const department = await Department.findOne({
+      where: { id: parseInt(departmentId, 10), organizationId: parseInt(organizationId, 10) },
+    });
+
+    // Check if the department was found
+    if (!department) {
+      return NextResponse.json({ message: 'Department not found' });
+    }
+
+    // Update the department with the new data
+    await department.update({ name });
+
+    // Send a successful response with the updated department data
+    return NextResponse.json({ message: 'Department updated successfully!', department });
+  } catch (error) {
+    // Log any errors that occur and send an error response
+    console.error(error);
+    return NextResponse.json({ message: 'Error updating department', error });
+  }
+}
 
