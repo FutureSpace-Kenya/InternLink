98e1267a4d4bc6af809edef8b285383048224e50
kingaranjoroge <111523863+kingaranjoroge@users.noreply.github.com>
2023-12-29 11:58:47 +0300

    updated DELETE function for departments

--- app/api/jobs_listings/departments/[organizationId]/route.js
+++ app/api/jobs_listings/departments/[organizationId]/route.js
@@ -50,3 +50,29 @@ export async function GET(request, { params }) {
       return NextResponse.json({ message: 'Error retrieving departments', error });
     }
 }
+
+// Next.js API route function for deleting all departments in the specified organization
+export async function DELETE(request, { params }) {
+  try {
+    // Extract organizationId from the request parameters
+    const { organizationId } = params;
+
+    // Delete all departments belonging to the specified organization
+    const deletedRows = await Department.destroy({
+      where: { organizationId: parseInt(organizationId, 10) },
+    });
+
+    // Check if any rows were deleted
+    if (deletedRows === 0) {
+      return NextResponse.json({ message: 'No departments found for the organization' });
+    }
+
+    // Send a successful response indicating the deletion
+    return NextResponse.json({ message: 'All departments deleted successfully!' });
+  } catch (error) {
+    // Log any errors that occur and send an error response
+    console.error(error);
+    return NextResponse.json({ message: 'Error deleting departments', error });
+  }
+}
+
