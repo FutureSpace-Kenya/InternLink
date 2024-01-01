95ddd3845808bd8ce91d0e6b04e3ff1a66bf46d5
Raccoon254 <tomsteve187@gmail.com>
2024-01-01 23:37:39 +0300

    Internship rows create

--- app/intern/dashboard/page.js
+++ app/intern/dashboard/page.js
@@ -280,7 +280,7 @@ const Dashboard = () => {
                     <div className="main flex mt-4">
 
                         <div className="left w-full md:w-3/4">
-                            <div className="internship-card border-b border-gray-800 pb-4 flex justify-between items-center flex-wrap gap-4">
+                            <div className="internship-card">
                                 <div className="start flex gap-4 justify-center">
                                     <div
                                         className="logo grid place-items-center bg-secondary text-white w-14 h-14 rounded-md">
@@ -311,6 +311,39 @@ const Dashboard = () => {
                                     </button>
                                 </div>
                             </div>
+
+                            <div className={`internship-card`}>
+                                <div className="start flex gap-4 justify-center">
+                                    <div
+                                        className="logo grid place-items-center bg-secondary text-white w-14 h-14 rounded-md">
+                                        <img className={'h-12 object-cover'}
+                                             src={'https://futurespace.vercel.app/resources/FsOutline.png'}
+                                             alt={'FutureSpace'}/>
+                                    </div>
+                                    <div className="info flex flex-col gap-1">
+                                        <h1 className="text-2xl font-bold">Software Engineering</h1>
+                                        <div className="text-[1rem] text-gray-500 flex flex-wrap gap-2 font-medium">
+                                            <p className="text-gray-800 font-bold">FutureSpace</p>
+                                            |
+                                            <p className="">Nairobi, Kenya</p>
+                                            |
+                                            <p className="">Ksh. 100,000</p>
+                                            |
+                                            <p className="">1 day ago</p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="button-container flex gap-3">
+                                    <button className="btn btn-outline">
+                                        Save
+                                    </button>
+                                    <button className="btn btn-primary">
+                                        Apply
+                                    </button>
+                                </div>
+                            </div>
+
                         </div>
 
                         <div className="right hidden md:w-1/4">
