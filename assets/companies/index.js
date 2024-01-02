7b11079335657478b858306ebe2469f2759fb9e9
jimmykimuyi-dev <eb14.55919.21@student.chuka.ac.ke>
2024-01-02 19:32:35 +0300

    Added the homepage header

---
+++ assets/companies/index.js
@@ -0,0 +1,19 @@
+import company1 from "./company1.webp";
+import company2 from "./company2.webp";
+import company3 from "./company3.webp";
+import company4 from "./company4.webp";
+import company5 from "./company5.webp";
+import company6 from "./company6.webp";
+import company7 from "./company7.jpg";
+
+const companies = [
+  company1,
+  company2,
+  company3,
+  company4,
+  company5,
+  company6,
+  company7,
+];
+
+export default companies;
