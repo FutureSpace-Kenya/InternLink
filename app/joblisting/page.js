92a53c38c0e861c0334e7320cbb23b314211f548
jimmykimuyi-dev <eb14.55919.21@student.chuka.ac.ke>
2024-01-04 13:59:48 +0300

    Improve the Job component UI

--- app/joblisting/page.js
+++ app/joblisting/page.js
@@ -1,24 +1,41 @@
-"use client";
-
 import React from "react";
 import data from "../utils/data";
 import "daisyui/dist/full.css";
 
 export default function JobListing() {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
+    <div className="grid grid-cols-1 gap-4 p-4">
+      {" "}
+      {/* Ensure only one column */}
       {data.map((job) => (
-        <div key={job.randomId} className="card bg-base-100 shadow-xl">
-          <figure className="px-10 pt-10">
-            <img
-              src={job.image}
-              alt="Role image"
-              className="rounded-xl border-solid border-3 border-[#B5B5B1]"
-            />
-            <p className="text-2xl"> Role Title</p>{" "}
-            {/* Assuming the base size is 1xl, 2xl will double the size */}
-          </figure>
+        <div key={job.randomId} className="card bg-base-100 shadow-xl w-full">
+          {" "}
+          {/* Set width to full */}
+          <div className="flex px-10 pt-10 items-center space-x-4">
+            <div className="internship-card m-1 p-1">
+              <div className="start flex gap-4 justify-center">
+                <div className="logo grid place-items-center bg-secondary text-white w-14 h-14 rounded-md">
+                  <img
+                    className="h-12 object-cover"
+                    src="https://futurespace.vercel.app/resources/FsOutline.png"
+                    alt="FutureSpace"
+                  />
+                </div>
+                <div className="info flex flex-col gap-1">
+                  <h1 className="text-2xl font-bold">Software Engineering</h1>
+                  <div className="text-[1rem] text-gray-500 flex flex-wrap gap-2 font-medium">
+                    <p className="text-gray-800 font-bold">FutureSpace</p>|
+                    <p>Nairobi, Kenya</p>|<p>Ksh. 100,000</p>|<p>1 day ago</p>
+                  </div>
+                </div>
+              </div>
 
+              <div className="button-container flex gap-3">
+                <button className="btn btn-outline">Save</button>
+                <button className="btn btn-primary">Apply</button>
+              </div>
+            </div>
+          </div>
           <div className="card-body">
             <h2 className="card-title">{job.title}</h2>
             <p className="text-lg">{job.description}</p>
