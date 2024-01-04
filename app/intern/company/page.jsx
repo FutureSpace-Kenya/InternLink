b1ad3bdb2a9a8aa19a9c03a1658f7a54f47b6479
deluxesande <deluxesande@gmail.com>
2024-01-04 16:42:55 +0300

    navigation to company page, id not configured

---
+++ app/intern/company/page.jsx
@@ -0,0 +1,38 @@
+"use client";
+import { useSession } from "next-auth/react";
+import Link from "next/link";
+
+const Company = () => {
+    const { data: session } = useSession();
+
+    if (!session)
+        return (
+            <div className="min-h-screen grid place-items-center w-full">
+                <span className="loading loading-ring loading-lg"></span>
+            </div>
+        );
+    return (
+        // Navbar
+        // Make it a component
+        <nav className="bg-green-500 p-4 flex justify-between items-center text-white">
+            <p className="text-2xl font-semibold">InternLink™</p>
+            <div className="flex gap-2">
+                <Link href="/intern/dashboard">Home</Link>
+                <button className="text-white bg-green-700 btn ring-1 ring-gray-100 ring-offset-1 btn-circle btn-ghost btn-sm">
+                    <i className="fa-solid fa-bell-slash"></i>
+                </button>
+                <button className="text-white bg-green-700 btn ring-1 ring-gray-100 ring-offset-1 btn-circle btn-ghost btn-sm">
+                    <i
+                        className={
+                            session.user
+                                ? "fa-solid fa-user"
+                                : "fas fa-sign-in-alt"
+                        }
+                    ></i>
+                </button>
+            </div>
+        </nav>
+    );
+};
+
+export default Company;
