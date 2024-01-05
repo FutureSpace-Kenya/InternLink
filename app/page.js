e13a550c19b1bbd6236246388b4751d6494b82eb
Raccoon254 <tomsteve187@gmail.com>
2024-01-05 07:56:41 +0300

    Homepage added image

--- app/page.js
+++ app/page.js
@@ -3,7 +3,8 @@ import LoginButton from "./components/LoginButton";
 export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-center p-24">
-            <div className="flex relative flex-col items-center">
+                    <img className="absolute top-0 opacity-60 h-full md:w-full object-cover" src="/student.jpg" alt="Student"/>
+            <div className="flex bg-student w-full relative flex-col items-center">
                 <div className="w-fit relative flex flex-col items-center">
                     <h1 className="text-6xl font-bold">
                      <span className="text-green-400">
@@ -11,8 +12,8 @@ export default function Home() {
                             </span>
                         Link&trade;
                     </h1>
-                    <div className="absolute top-[55px] right-0 mb-4 text-xs font-medium text-orange-800">
-                        By <a className={'text-blue-500'} href="https://futurespace.vercel.app">FutureSpace</a>
+                    <div className="absolute top-[55px] right-0 mb-4 text-sm font-medium text-orange-800">
+                        By <a className={'text-blue-900'} href="https://futurespace.vercel.app">FutureSpace</a>
                     </div>
                 </div>
                 <p className="text-2xl text-center">
