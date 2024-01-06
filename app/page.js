ecc5dc70a356d4b35295e38953882653518e0ac5
Raccoon254 <tomsteve187@gmail.com>
2024-01-07 00:03:17 +0300

    Card Line fix

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
             <main className="flex min-h-screen flex-col items-center justify-center">
                 <img className="absolute blur-[1px] top-0 filter backdrop-filter h-full md:w-full object-cover"
                      src="/student.jpg" alt="Student"/>
-                <div className="absolute custom-gradient top-0 left-0 w-full h-full">
+                <div className="absolute -mb-3 custom-gradient top-2 left-0 w-full h-full">
 
                 </div>
                 <div className="flex bg-student w-full m-2 relative flex-col items-center">
@@ -67,32 +67,113 @@ export default function Home() {
                 </div>
 
 
-                <div className="left w-full p-4 sm:w-1/2">
+                <div className="left flex gap-10 w-full p-4 sm:w-1/2">
 
-                    <div className="custom-c relative">
-                        <div
-                            className="cicle grid place-items-center  from-green-700 to-transparent rounded-full w-20 h-20">
-                            <i className={'fas fa-user-graduate text-4xl text-white'}></i>
-                        </div>
-                        <svg className="w-24 top-3 absolute h-24">
-                            <defs>
-                                <linearGradient id="gradientStroke" x1="0%" y1="100%" x2="0%" y2="0%">
-                                    <stop offset="10%" style={{stopColor: "rgb(74, 222, 128,0)", stopOpacity: 0}}/>
-                                    <stop offset="70%" style={{stopColor: "rgb(74, 222, 128,0.5)", stopOpacity: 0.5}}/>
-                                    <stop offset="100%" style={{stopColor: "rgb(74, 222, 128)", stopOpacity: 1}}/>
-                                </linearGradient>
-                            </defs>
-                            <circle cx="50%" cy="50%" r="40%" stroke="url(#gradientStroke)" strokeWidth="3"
-                                    fill="transparent"/>
-                        </svg>
+                    <div className="right flex flex-col gap-10">
+                        <div className="custom-c relative">
+                            <div
+                                className="cicle grid place-items-center  from-green-700 to-transparent rounded-full w-20 h-20">
+                                <i className="fa-solid text-3xl text-white fa-shield-halved"></i>
+                            </div>
+                            <svg className="w-24 top-6 absolute h-24">
+                                <defs>
+                                    <linearGradient id="gradientStroke" x1="0%" y1="100%" x2="0%" y2="0%">
+                                        <stop offset="5%" style={{stopColor: "rgb(74, 222, 128,0)", stopOpacity: 0}}/>
+                                        <stop offset="70%"
+                                              style={{stopColor: "rgb(74, 222, 128,0.5)", stopOpacity: 0.5}}/>
+                                        <stop offset="100%"
+                                              style={{stopColor: "rgb(74, 222, 128,0.9)", stopOpacity: 1}}/>
+                                    </linearGradient>
+                                </defs>
+                                <circle cx="50%" cy="50%" r="40%" stroke="url(#gradientStroke)" strokeWidth="4"
+                                        fill="transparent"/>
+                            </svg>
 
 
-                        <span className={'text-xs font-medium'}>
+                            <span className={'text-xs font-medium'}>
                             Wide reach
                         </span>
-                    </div>
-                </div>
+                        </div>
 
+                        <div className="custom-c relative">
+                            <div
+                                className="cicle grid place-items-center  from-green-700 to-transparent rounded-full w-20 h-20">
+                                <i className="fa-solid text-3xl text-white fa-shield-halved"></i>
+                            </div>
+                            <svg className="w-24 top-6 absolute h-24">
+                                <defs>
+                                    <linearGradient id="gradientStroke" x1="0%" y1="100%" x2="0%" y2="0%">
+                                        <stop offset="5%" style={{stopColor: "rgb(74, 222, 128,0)", stopOpacity: 0}}/>
+                                        <stop offset="70%"
+                                              style={{stopColor: "rgb(74, 222, 128,0.5)", stopOpacity: 0.5}}/>
+                                        <stop offset="100%"
+                                              style={{stopColor: "rgb(74, 222, 128,0.9)", stopOpacity: 1}}/>
+                                    </linearGradient>
+                                </defs>
+                                <circle cx="50%" cy="50%" r="40%" stroke="url(#gradientStroke)" strokeWidth="4"
+                                        fill="transparent"/>
+                            </svg>
+
+
+                            <span className={'text-xs font-medium'}>
+                            Wide reach
+                        </span>
+                        </div>
+                    </div>
+
+                    <div className="right -mt-14 flex flex-col gap-10">
+                        <div className="custom-c relative">
+                            <div
+                                className="cicle grid place-items-center  from-green-700 to-transparent rounded-full w-20 h-20">
+                                <i className="fa-solid text-3xl text-white fa-shield-halved"></i>
+                            </div>
+                            <svg className="w-24 top-6 absolute h-24">
+                                <defs>
+                                    <linearGradient id="gradientStroke" x1="0%" y1="100%" x2="0%" y2="0%">
+                                        <stop offset="5%" style={{stopColor: "rgb(74, 222, 128,0)", stopOpacity: 0}}/>
+                                        <stop offset="70%"
+                                              style={{stopColor: "rgb(74, 222, 128,0.5)", stopOpacity: 0.5}}/>
+                                        <stop offset="100%"
+                                              style={{stopColor: "rgb(74, 222, 128,0.9)", stopOpacity: 1}}/>
+                                    </linearGradient>
+                                </defs>
+                                <circle cx="50%" cy="50%" r="40%" stroke="url(#gradientStroke)" strokeWidth="4"
+                                        fill="transparent"/>
+                            </svg>
+
+
+                            <span className={'text-xs font-medium'}>
+                            Wide reach
+                        </span>
+                        </div>
+
+                        <div className="custom-c relative">
+                            <div
+                                className="cicle grid place-items-center  from-green-700 to-transparent rounded-full w-20 h-20">
+                                <i className="fa-solid text-3xl text-white fa-shield-halved"></i>
+                            </div>
+                            <svg className="w-24 top-6 absolute h-24">
+                                <defs>
+                                    <linearGradient id="gradientStroke" x1="0%" y1="100%" x2="0%" y2="0%">
+                                        <stop offset="5%" style={{stopColor: "rgb(74, 222, 128,0)", stopOpacity: 0}}/>
+                                        <stop offset="70%"
+                                              style={{stopColor: "rgb(74, 222, 128,0.5)", stopOpacity: 0.5}}/>
+                                        <stop offset="100%"
+                                              style={{stopColor: "rgb(74, 222, 128,0.9)", stopOpacity: 1}}/>
+                                    </linearGradient>
+                                </defs>
+                                <circle cx="50%" cy="50%" r="40%" stroke="url(#gradientStroke)" strokeWidth="4"
+                                        fill="transparent"/>
+                            </svg>
+
+
+                            <span className={'text-xs font-medium'}>
+                            Wide reach
+                        </span>
+                        </div>
+                    </div>
+
+                </div>
             </div>
         </>
     )
