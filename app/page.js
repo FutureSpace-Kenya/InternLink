b23141842c34ea6a90fce39c7c7d4f650fc20a8e
Raccoon254 <tomsteve187@gmail.com>
2024-01-06 23:53:29 +0300

    Created custom ad card

--- app/page.js
+++ app/page.js
@@ -1,61 +1,64 @@
 import React from 'react'
 import LoginButton from "./components/LoginButton";
+
 export default function Home() {
     return (
         <>
-        <main className="flex min-h-screen flex-col items-center justify-center">
-                    <img className="absolute blur-[1px] top-0 filter backdrop-filter h-full md:w-full object-cover" src="/student.jpg" alt="Student"/>
-            <div className="absolute custom-gradient top-0 left-0 w-full h-full">
+            <main className="flex min-h-screen flex-col items-center justify-center">
+                <img className="absolute blur-[1px] top-0 filter backdrop-filter h-full md:w-full object-cover"
+                     src="/student.jpg" alt="Student"/>
+                <div className="absolute custom-gradient top-0 left-0 w-full h-full">
 
-            </div>
-            <div className="flex bg-student w-full m-2 relative flex-col items-center">
-                <div className="w-fit relative flex flex-col items-center">
-                    <h1 className="text-6xl font-bold">
+                </div>
+                <div className="flex bg-student w-full m-2 relative flex-col items-center">
+                    <div className="w-fit relative flex flex-col items-center">
+                        <h1 className="text-6xl font-bold">
                      <span className="text-green-400">
                                  Intern
                             </span>
-                        Link&trade;
-                    </h1>
-                    <div className="absolute top-[55px] right-0 mb-4 text-sm font-medium text-orange-800">
-                        By <a className={'text-blue-900'} href="https://futurespace.vercel.app">FutureSpace</a>
+                            Link&trade;
+                        </h1>
+                        <div className="absolute top-[55px] right-0 mb-4 text-sm font-medium text-orange-800">
+                            By <a className={'text-blue-900'} href="https://futurespace.vercel.app">FutureSpace</a>
+                        </div>
                     </div>
-                </div>
-                <p className="text-2xl text-center">
-                    <br/>
-                    Welcome to the future of internships
-                </p>
+                    <p className="text-2xl text-center">
+                        <br/>
+                        Welcome to the future of internships
+                    </p>
 
 
-                <div className="flex w-full items-center justify-center space-x-4 mt-8">
-                    <a href="auth/login">
-                        <button className="btn btn-outline btn-primary ring-1 ring-secondary ring-offset-1">
-                            <i className={'fas fa-sign-in-alt'}></i> Sign In
-                        </button>
-                    </a>
-                    <a href="auth/register">
-                        <button className="btn btn-secondary ring-1 ring-secondary ring-offset-1">
-                            <i className={'fas fa-user-plus'}></i> Sign Up
-                        </button>
-                    </a>
-                </div>
-            </div>
-            <div className="absolute bottom-5 flex flex-col items-center justify-center scroll">
-                <div className="scroll-downs">
-                    <div className="mousey">
-                        <div className="scroller"></div>
+                    <div className="flex w-full items-center justify-center space-x-4 mt-8">
+                        <a href="auth/login">
+                            <button className="btn btn-outline btn-primary ring-1 ring-secondary ring-offset-1">
+                                <i className={'fas fa-sign-in-alt'}></i> Sign In
+                            </button>
+                        </a>
+                        <a href="auth/register">
+                            <button className="btn btn-secondary ring-1 ring-secondary ring-offset-1">
+                                <i className={'fas fa-user-plus'}></i> Sign Up
+                            </button>
+                        </a>
                     </div>
                 </div>
-                <div className="mt-24 text-rgba font-semibold">
-                    Scroll Down
+                <div className="absolute bottom-5 flex flex-col items-center justify-center scroll">
+                    <div className="scroll-downs">
+                        <div className="mousey">
+                            <div className="scroller"></div>
+                        </div>
+                    </div>
+                    <div className="mt-24 text-rgba font-semibold">
+                        Scroll Down
+                    </div>
                 </div>
-            </div>
-        </main>
+            </main>
             <div className="flex flex-col sm:flex-row h-96 items-center justify-center w-full">
                 <div className="right w-full p-4 sm:w-1/2">
-                    <h1 className={'my-2'}>Discover <span className={'text-green-500'}>Intern</span>Link<span className={''}>&trade;</span>
+                    <h1 className={'my-2'}>Discover <span className={'text-green-500'}>Intern</span>Link<span
+                        className={''}>&trade;</span>
                     </h1>
                     <p>
-                    InternLink streamlines the journey for students to secure internships and attachments. Our
+                        InternLink streamlines the journey for students to secure internships and attachments. Our
                         platform simplifies the application process, enabling students to effortlessly explore and apply
                         for opportunities. Simultaneously, it empowers employers to effortlessly discover and recruit
                         top talent for their internship programs and attachment roles. With InternLink, connecting the
@@ -64,8 +67,30 @@ export default function Home() {
                 </div>
 
 
-                <div className="left w-full sm:w-1/2">
+                <div className="left w-full p-4 sm:w-1/2">
 
+                    <div className="custom-c relative">
+                        <div
+                            className="cicle grid place-items-center  from-green-700 to-transparent rounded-full w-20 h-20">
+                            <i className={'fas fa-user-graduate text-4xl text-white'}></i>
+                        </div>
+                        <svg className="w-24 top-3 absolute h-24">
+                            <defs>
+                                <linearGradient id="gradientStroke" x1="0%" y1="100%" x2="0%" y2="0%">
+                                    <stop offset="10%" style={{stopColor: "rgb(74, 222, 128,0)", stopOpacity: 0}}/>
+                                    <stop offset="70%" style={{stopColor: "rgb(74, 222, 128,0.5)", stopOpacity: 0.5}}/>
+                                    <stop offset="100%" style={{stopColor: "rgb(74, 222, 128)", stopOpacity: 1}}/>
+                                </linearGradient>
+                            </defs>
+                            <circle cx="50%" cy="50%" r="40%" stroke="url(#gradientStroke)" strokeWidth="3"
+                                    fill="transparent"/>
+                        </svg>
+
+
+                        <span className={'text-xs font-medium'}>
+                            Wide reach
+                        </span>
+                    </div>
                 </div>
 
             </div>
