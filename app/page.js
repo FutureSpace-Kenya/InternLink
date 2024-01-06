ea2e2a7ceab1a6581a795e6fda87cd7d84ed2d3f
Raccoon254 <tomsteve187@gmail.com>
2024-01-07 00:38:06 +0300

    Card completion

--- app/page.js
+++ app/page.js
@@ -18,7 +18,7 @@ export default function Home() {
                             </span>
                             Link&trade;
                         </h1>
-                        <div className="absolute top-[55px] right-0 mb-4 text-sm font-medium text-orange-800">
+                        <div className="absolute top-[55px] right-0 mb-4 text-sm font-semibold text-orange-800">
                             By <a className={'text-blue-900'} href="https://futurespace.vercel.app">FutureSpace</a>
                         </div>
                     </div>
@@ -52,7 +52,7 @@ export default function Home() {
                     </div>
                 </div>
             </main>
-            <div className="flex flex-col sm:flex-row h-96 items-center justify-center w-full">
+            <div className="flex flex-col sm:flex-row items-center justify-center w-full">
                 <div className="right w-full p-4 sm:w-1/2">
                     <h1 className={'my-2'}>Discover <span className={'text-green-500'}>Intern</span>Link<span
                         className={''}>&trade;</span>
@@ -67,42 +67,42 @@ export default function Home() {
                 </div>
 
 
-                <div className="left flex gap-10 w-full p-4 sm:w-1/2">
+                <div className="left text-green-800 flex items-center justify-center pt-4 gap-6 w-full p-4 sm:w-1/2">
 
-                    <div className="right flex flex-col gap-10">
-                        <div className="custom-c relative">
+                    <div className="right flex sm:mt-10 flex-col gap-6">
+                        <div className="custom-c-mod relative">
                             <div
                                 className="cicle grid place-items-center  from-green-700 to-transparent rounded-full w-20 h-20">
-                                <i className="fa-solid text-3xl text-white fa-shield-halved"></i>
+                                <i className="fa-solid text-3xl fa-shield"></i>
                             </div>
                             <svg className="w-24 top-6 absolute h-24">
                                 <defs>
-                                    <linearGradient id="gradientStroke" x1="0%" y1="100%" x2="0%" y2="0%">
-                                        <stop offset="5%" style={{stopColor: "rgb(74, 222, 128,0)", stopOpacity: 0}}/>
+                                    <linearGradient id="gradientStroke1" x1="0%" y1="100%" x2="0%" y2="0%">
+                                        <stop offset="5%" style={{stopColor: "rgb(206,112,25,0)", stopOpacity: 0}}/>
                                         <stop offset="70%"
-                                              style={{stopColor: "rgb(74, 222, 128,0.5)", stopOpacity: 0.5}}/>
+                                              style={{stopColor: "rgb(206,112,25)", stopOpacity: 0.5}}/>
                                         <stop offset="100%"
-                                              style={{stopColor: "rgb(74, 222, 128,0.9)", stopOpacity: 1}}/>
+                                              style={{stopColor: "rgba(206,112,25,0.9)", stopOpacity: 1}}/>
                                     </linearGradient>
                                 </defs>
-                                <circle cx="50%" cy="50%" r="40%" stroke="url(#gradientStroke)" strokeWidth="4"
+                                <circle cx="50%" cy="50%" r="40%" stroke="url(#gradientStroke1)" strokeWidth="4"
                                         fill="transparent"/>
                             </svg>
 
 
-                            <span className={'text-xs font-medium'}>
-                            Wide reach
+                            <span className={'text-sm font-semibold'}>
+                            Safe & Reliable
                         </span>
                         </div>
 
                         <div className="custom-c relative">
-                            <div
+                        <div
                                 className="cicle grid place-items-center  from-green-700 to-transparent rounded-full w-20 h-20">
-                                <i className="fa-solid text-3xl text-white fa-shield-halved"></i>
-                            </div>
+                            <i className="fa-solid text-3xl fa-rocket"></i>
+                        </div>
                             <svg className="w-24 top-6 absolute h-24">
                                 <defs>
-                                    <linearGradient id="gradientStroke" x1="0%" y1="100%" x2="0%" y2="0%">
+                                    <linearGradient id="gradientStroke2" x1="0%" y1="100%" x2="0%" y2="0%">
                                         <stop offset="5%" style={{stopColor: "rgb(74, 222, 128,0)", stopOpacity: 0}}/>
                                         <stop offset="70%"
                                               style={{stopColor: "rgb(74, 222, 128,0.5)", stopOpacity: 0.5}}/>
@@ -110,26 +110,26 @@ export default function Home() {
                                               style={{stopColor: "rgb(74, 222, 128,0.9)", stopOpacity: 1}}/>
                                     </linearGradient>
                                 </defs>
-                                <circle cx="50%" cy="50%" r="40%" stroke="url(#gradientStroke)" strokeWidth="4"
+                                <circle cx="50%" cy="50%" r="40%" stroke="url(#gradientStroke2)" strokeWidth="4"
                                         fill="transparent"/>
                             </svg>
 
 
-                            <span className={'text-xs font-medium'}>
-                            Wide reach
+                            <span className={'text-sm font-semibold'}>
+                            A FutureSpace product
                         </span>
                         </div>
                     </div>
 
-                    <div className="right -mt-14 flex flex-col gap-10">
+                    <div className="right sm:-mt-14 flex flex-col gap-6">
                         <div className="custom-c relative">
                             <div
                                 className="cicle grid place-items-center  from-green-700 to-transparent rounded-full w-20 h-20">
-                                <i className="fa-solid text-3xl text-white fa-shield-halved"></i>
+                                <i className="fa-solid text-3xl fa-clock-rotate-left"></i>
                             </div>
                             <svg className="w-24 top-6 absolute h-24">
                                 <defs>
-                                    <linearGradient id="gradientStroke" x1="0%" y1="100%" x2="0%" y2="0%">
+                                    <linearGradient id="gradientStroke3" x1="0%" y1="100%" x2="0%" y2="0%">
                                         <stop offset="5%" style={{stopColor: "rgb(74, 222, 128,0)", stopOpacity: 0}}/>
                                         <stop offset="70%"
                                               style={{stopColor: "rgb(74, 222, 128,0.5)", stopOpacity: 0.5}}/>
@@ -137,38 +137,38 @@ export default function Home() {
                                               style={{stopColor: "rgb(74, 222, 128,0.9)", stopOpacity: 1}}/>
                                     </linearGradient>
                                 </defs>
-                                <circle cx="50%" cy="50%" r="40%" stroke="url(#gradientStroke)" strokeWidth="4"
+                                <circle cx="50%" cy="50%" r="40%" stroke="url(#gradientStroke3)" strokeWidth="4"
                                         fill="transparent"/>
                             </svg>
 
 
-                            <span className={'text-xs font-medium'}>
-                            Wide reach
+                            <span className={'text-sm font-semibold'}>
+                            Time saving
                         </span>
                         </div>
 
-                        <div className="custom-c relative">
+                        <div className="custom-c-mod relative">
                             <div
                                 className="cicle grid place-items-center  from-green-700 to-transparent rounded-full w-20 h-20">
-                                <i className="fa-solid text-3xl text-white fa-shield-halved"></i>
+                                <i className="fa-regular text-3xl fa-face-grin-beam"></i>
                             </div>
                             <svg className="w-24 top-6 absolute h-24">
                                 <defs>
-                                    <linearGradient id="gradientStroke" x1="0%" y1="100%" x2="0%" y2="0%">
-                                        <stop offset="5%" style={{stopColor: "rgb(74, 222, 128,0)", stopOpacity: 0}}/>
+                                    <linearGradient id="gradientStroke4" x1="0%" y1="100%" x2="0%" y2="0%">
+                                        <stop offset="5%" style={{stopColor: "rgb(206,112,25,0)", stopOpacity: 0}}/>
                                         <stop offset="70%"
-                                              style={{stopColor: "rgb(74, 222, 128,0.5)", stopOpacity: 0.5}}/>
+                                              style={{stopColor: "rgb(206,112,25)", stopOpacity: 0.5}}/>
                                         <stop offset="100%"
-                                              style={{stopColor: "rgb(74, 222, 128,0.9)", stopOpacity: 1}}/>
+                                              style={{stopColor: "rgba(206,112,25,0.9)", stopOpacity: 1}}/>
                                     </linearGradient>
                                 </defs>
-                                <circle cx="50%" cy="50%" r="40%" stroke="url(#gradientStroke)" strokeWidth="4"
+                                <circle cx="50%" cy="50%" r="40%" stroke="url(#gradientStroke4)" strokeWidth="4"
                                         fill="transparent"/>
                             </svg>
 
 
-                            <span className={'text-xs font-medium'}>
-                            Wide reach
+                            <span className={'text-sm font-semibold'}>
+                            Free to use
                         </span>
                         </div>
                     </div>
