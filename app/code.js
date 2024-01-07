fc10d30ab37627202c82753fb052777fdcb4890c
jimmykimunyi-dev <eb14.55919.21@student.chuka.ac.ke>
2024-01-07 17:55:39 +0300

    Fixing homepage

---
+++ app/code.js
@@ -0,0 +1,160 @@
+<div className="flex flex-col gap-2 m-4 p-4">
+  <div className="custom-c-mod relative">
+    <div className="cicle grid place-items-center  from-green-700 to-transparent rounded-full w-20 h-20">
+      <i className="fa-solid text-3xl fa-shield"></i>
+    </div>
+    <svg className="w-24 top-6 absolute h-24">
+      <defs>
+        <linearGradient id="gradientStroke1" x1="0%" y1="100%" x2="0%" y2="0%">
+          <stop
+            offset="5%"
+            style={{ stopColor: "rgb(206,112,25,0)", stopOpacity: 0 }}
+          />
+          <stop
+            offset="70%"
+            style={{ stopColor: "rgb(206,112,25)", stopOpacity: 0.5 }}
+          />
+          <stop
+            offset="100%"
+            style={{
+              stopColor: "rgba(206,112,25,0.9)",
+              stopOpacity: 1,
+            }}
+          />
+        </linearGradient>
+      </defs>
+      <circle
+        cx="50%"
+        cy="50%"
+        r="40%"
+        stroke="url(#gradientStroke1)"
+        strokeWidth="4"
+        fill="transparent"
+      />
+    </svg>
+
+    <span className="text-sm font-semibold">Safe & Reliable</span>
+  </div>
+
+  <div className="custom-c relative">
+    <div className="cicle grid place-items-center  from-green-700 to-transparent rounded-full w-20 h-20">
+      <i className="fa-solid text-3xl fa-rocket"></i>
+    </div>
+    <svg className="w-24 top-6 absolute h-24">
+      <defs>
+        <linearGradient id="gradientStroke2" x1="0%" y1="100%" x2="0%" y2="0%">
+          <stop
+            offset="5%"
+            style={{
+              stopColor: "rgb(74, 222, 128,0)",
+              stopOpacity: 0,
+            }}
+          />
+          <stop
+            offset="70%"
+            style={{
+              stopColor: "rgb(74, 222, 128,0.5)",
+              stopOpacity: 0.5,
+            }}
+          />
+          <stop
+            offset="100%"
+            style={{
+              stopColor: "rgb(74, 222, 128,0.9)",
+              stopOpacity: 1,
+            }}
+          />
+        </linearGradient>
+      </defs>
+      <circle
+        cx="50%"
+        cy="50%"
+        r="40%"
+        stroke="url(#gradientStroke2)"
+        strokeWidth="4"
+        fill="transparent"
+      />
+    </svg>
+
+    <span className={"text-sm font-semibold"}>A FutureSpace product</span>
+  </div>
+  <div className="custom-c relative">
+    <div className="cicle grid place-items-center  from-green-700 to-transparent rounded-full w-20 h-20">
+      <i className="fa-solid text-3xl fa-clock-rotate-left"></i>
+    </div>
+    <svg className="w-24 top-6 absolute h-24">
+      <defs>
+        <linearGradient id="gradientStroke3" x1="0%" y1="100%" x2="0%" y2="0%">
+          <stop
+            offset="5%"
+            style={{
+              stopColor: "rgb(74, 222, 128,0)",
+              stopOpacity: 0,
+            }}
+          />
+          <stop
+            offset="70%"
+            style={{
+              stopColor: "rgb(74, 222, 128,0.5)",
+              stopOpacity: 0.5,
+            }}
+          />
+          <stop
+            offset="100%"
+            style={{
+              stopColor: "rgb(74, 222, 128,0.9)",
+              stopOpacity: 1,
+            }}
+          />
+        </linearGradient>
+      </defs>
+      <circle
+        cx="50%"
+        cy="50%"
+        r="40%"
+        stroke="url(#gradientStroke3)"
+        strokeWidth="4"
+        fill="transparent"
+      />
+    </svg>
+
+    <span className={"text-sm font-semibold"}>Time saving</span>
+  </div>
+
+  <div className="custom-c-mod relative">
+    <div className="cicle grid place-items-center  from-green-700 to-transparent rounded-full w-20 h-20">
+      <i className="fa-regular text-3xl fa-face-grin-beam"></i>
+    </div>
+    <svg className="w-24 top-6 absolute h-24">
+      <defs>
+        <linearGradient id="gradientStroke4" x1="0%" y1="100%" x2="0%" y2="0%">
+          <stop
+            offset="5%"
+            style={{ stopColor: "rgb(206,112,25,0)", stopOpacity: 0 }}
+          />
+          <stop
+            offset="70%"
+            style={{ stopColor: "rgb(206,112,25)", stopOpacity: 0.5 }}
+          />
+          <stop
+            offset="100%"
+            style={{
+              stopColor: "rgba(206,112,25,0.9)",
+              stopOpacity: 1,
+            }}
+          />
+        </linearGradient>
+      </defs>
+      <circle
+        cx="50%"
+        cy="50%"
+        r="40%"
+        stroke="url(#gradientStroke4)"
+        strokeWidth="4"
+        fill="transparent"
+      />
+    </svg>
+
+    <span className={"text-sm font-semibold"}>Free to use</span>
+  </div>
+</div>;
